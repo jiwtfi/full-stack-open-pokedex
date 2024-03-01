839cd8adee53bca95d3f518cfab4ac7dd928efd6
Junya Iwata <junya.iwata@aalto.fi>
2024-03-01 19:57:18 +0200

    exercise 11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -21,19 +22,15 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
+      2
     ],
+    // "linebreak-style": [
+    //   "error",
+    //   "unix"
+    // ],
     "quotes": [
       "error",
       "single"
-    ],
-    "semi": [
-      "error",
-      "never"
     ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
@@ -43,7 +40,6 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
     "react/prop-types": 0
   }
 }
